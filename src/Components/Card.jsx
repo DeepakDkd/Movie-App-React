d42b30f219b863590daed245e3b849a55b51f733
Deepak Dhakad <deepakdkd1188@gmail.com>
2024-01-25 01:31:57 +0530

    update functionality

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -8,45 +8,54 @@ function Card() {
     console.log(movie)
 
     return movie ? (
-        <div className='card'>
-            <div className="moviePoster">
-                <img src={movie.Poster} alt={movie.Title} />
+        <div className='card' style={{ backgroundImage: `url(${movie.Poster})` }}>
+            <div className="container">
+
+                <div className="moviePoster">
+                    <img src={movie.Poster} alt={movie.Title} />
+                </div>
+                <div className="movieInfo">
+                    
+                    <h2>{movie.Title} </h2>
+                    {
+                        movie && Object.keys(movie).map((key, index) => (
+                            key === 'Poster' || movie[key] == 'N/A' || key === 'Title' || key === 'Ratings' || key === 'Response' || key === 'imdbID' ? (
+                                null
+                            ) : <p><strong>{`${key} : `}</strong> {`${movie[key]}`}</p>
+
+                        ))
+                    }
+                    {
+                        movie.Ratings.map((rating, index) => (
+                            <p key={index}>
+
+                                <strong>Ratings :  </strong>{rating.Source} -  {rating.Value}
+                            </p>
+                        ))
+                    }
+
+                    <br/>
+
+                    {
+                        movie?.totalSeasons &&
+                        <div className='seasonBtn'>
+                            <h3>{movie.totalSeasons} seasons available</h3>
+
+                            {
+
+                                Array.from({ length: movie.totalSeasons }).map((j, i) => (
+                                    <button>{i + 1}</button>
+                                ))
+
+                            }
+
+
+                        </div>
+                    }
+
+                </div>
             </div>
-            <div className="movieInfo">
-                <h2>{movie.Title} ({movie.Year})</h2>
-                <p>
-                    <strong>Director:</strong> {movie.Director}
-                    <br />
-                    <strong>Genre:</strong> {movie.Genre}
-                    <br />
-                    <strong>Released:</strong> {movie.Released}
-                    <br />
-                    <strong>Runtime:</strong> {movie.Runtime}
-                    <br />
-                    <strong>Rated:</strong> {movie.Rated}
-                    <br />
-                    <strong>Language:</strong> {movie.Language}
-                    <br />
-                    <strong>Actors:</strong> {movie.Actors}
-                    <br />
-                    <strong>Box Office:</strong> {movie.BoxOffice}
-                </p>
-                <h3>Plot</h3>
-                <p>{movie.Plot}</p>
-                <h3>Ratings</h3>
-                <ul>
-                    {movie.Ratings.map((rating, index) => (
-                        <li key={index}>
-                            <strong>{rating.Source}:</strong> {rating.Value}
-                        </li>
-                    ))}
-                </ul>
-                <h3>Awards</h3>
-                <p>{movie.Awards}</p>
-                <h3>IMDb Votes</h3>
-                <p>{movie.imdbVotes}</p>
-                {/* Add more details as needed */}
-            </div>
+
         </div>
     ) : null;
 }
--- src/Hooks/useMovieData.js
+++ src/Hooks/useMovieData.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react'
 function useMovieData({param,movieID}) {
     console.log(param)
     console.log(movieID)
-    // movieID = "tt0107362"
     const [movieData , setMovieData] = useState(null);
     useEffect(() => {
         fetch(`https://www.omdbapi.com/?apikey=14402bcc${param ? '&s=' + param:""} ${movieID ? '&i=' + movieID : ""}`)
