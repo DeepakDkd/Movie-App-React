664d5f11ebd2eec95b867068d04680cc243c8b05
Deepak Dhakad <deepakdkd1188@gmail.com>
2024-02-08 22:15:03 +0530

    card modified

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -17,7 +17,7 @@ function Card() {
 
 
                     <div className="moviePoster">
-                        <img src={movie.Poster} alt={movie.Title} />
+                        <img src={movie.Poster == 'N/A' ? "https://t3.ftcdn.net/jpg/04/62/93/66/360_F_462936689_BpEEcxfgMuYPfTaIAOC1tCDurmsno7Sp.jpg" : movie.Poster} alt={movie.Title} />
                     </div>
                     <div className="movieInfo" >
 
@@ -41,26 +41,29 @@ function Card() {
 
                         <br />
 
-                        
+
 
                     </div>
                 </div>
                 {
-                            movie?.totalSeasons &&
-                            <div className='seasonBtn'>
-                                <h3>{movie.totalSeasons} season available</h3>
-
-                                {
-
-                                    Array.from({ length: movie.totalSeasons }).map((j, i) => (
-                                        <button onClick={() => navigate(`/movie/${movie.imdbID}/${i + 1}`)} key={i + 1}>{i + 1}</button>
-                                    ))
-
-                                }
+                    movie?.totalSeasons &&
+                    <div className='seasonBtn'>
+                        <h3>{movie.totalSeasons} season available</h3>
+                        <div>
 
 
-                            </div>
-                        }
+                            {
+
+                                Array.from({ length: movie.totalSeasons }).map((_, i) => (
+                                    <button onClick={() => navigate(`/movie/${movie.imdbID}/${i + 1}`)} key={i + 1}>{i + 1}</button>
+                                ))
+
+                            }
+                        </div>
+
+
+                    </div>
+                }
                 <Season />
             </div>
         // </div>
