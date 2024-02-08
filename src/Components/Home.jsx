7765fdf14513a8fa4baf59bd62e510b010ddaaff
Deepak Dhakad <deepakdkd1188@gmail.com>
2024-02-08 22:15:41 +0530

    Website name changed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,16 +7,12 @@ function Home() {
             <div className='home-bg'></div>
 
             <div className="intro-section">
-                <h2>Welcome to CineStream!</h2>
+                <h2>Welcome to CineScore!</h2>
                 <p>Discover, review, and share your favorite movies and series. Use the search bar above to find your next movie night pick!</p>
             </div>
 
             <Search />
 
-            <div className="popular-section">
-                <h3>Popular Picks</h3>
-
-            </div>
 
         </div >
     )
