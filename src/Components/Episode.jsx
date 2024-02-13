4d6532c100157375f443e822346a2a4824f0cac9
Deepak Dhakad <deepakdkd1188@gmail.com>
2024-02-14 03:34:59 +0530

    issue fixed!

--- src/Components/Episode.jsx
+++ src/Components/Episode.jsx
@@ -1,54 +1,53 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import useMovieData from '../Hooks/useMovieData';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import useMovieData from "../Hooks/useMovieData";
 function Episode() {
-    const { movieID, episode } = useParams();
-    const [episodeData, setEpisodeData] = useState()
-    const data = useMovieData({ movieID, episode })
-    // useEffect(()=>{
-    // if(data)setEpisodeData(data)
-    // console.log(episodeData)
-    // },[data])
-    console.log(data)
-    console.log(episode)    
-    return data ? (
-        <div className="episodeBg" style={{ backgroundImage: `url(${data.Poster})` }}>
+  const { movieID, episode } = useParams();
+  const [episodeData, setEpisodeData] = useState();
+  const data = useMovieData({ movieID, episode });
+  // useEffect(()=>{
+  // if(data)setEpisodeData(data)
+  // console.log(episodeData)
+  // },[data])
+  console.log(data);
+  console.log(episode);
+  return data ? (
+    <div
+      className="episodeBg"
+      style={{ backgroundImage: `url(${data.Poster})` }}
+    >
+      <div className="episode">
+        <div className="episodeData">
+          <img src={data.Poster} alt="" />
 
-            <div className="episode">
-                <h1>{data.Title}</h1>
-                <div className="episodeData">
-
-                    <img src={data.Poster} alt="" />
-
-                    <div className='episodeInfo'>
-                        {
-
-                            data &&
-                            Object.keys(data).map((key, index, obj) => (
-
-                                key != "Poster" && key != "Response" && key != "imdbID" && data[key] != "N/A" && key != "Ratings" &&
-                                <p key={index}>
-
-                                    <strong>{`${key} : `}</strong>
-                                    {`${data[key]}`}
-
-                                </p>
-                            ))
-                        }
-                        {
-
-                            data.Ratings.map((rating) => (
-                                <p>
-                                    <strong>{`Source: ${rating.Source}`}</strong>: {`${rating.Value}`}
-                                </p>
-                            ))
-                        }
-                    </div>
-                </div>
-            </div>
-
+          <div className="episodeInfo">
+            <h1 >{data.Title}</h1>
+            {data &&
+              Object.keys(data).map(
+                (key, index, obj) =>
+                  key != "Poster" &&
+                  key != "Title" &&
+                  key != "Response" &&
+                  key != "imdbID" &&
+                  data[key] != "N/A" &&
+                  key != "Ratings" && (
+                    <p key={index}>
+                      <strong>{`${key} : `}</strong>
+                      {`${data[key]}`}
+                    </p>
+                  )
+              )}
+            {data.Ratings.map((rating) => (
+              <p>
+                <strong>{`Source: ${rating.Source}`}</strong>:{" "}
+                {`${rating.Value}`}
+              </p>
+            ))}
+          </div>
         </div>
-    ) : null
+      </div>
+    </div>
+  ) : null;
 }
 
-export default Episode
+export default Episode;
