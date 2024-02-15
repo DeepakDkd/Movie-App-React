272e26f824d826844f71f8597cda47b776d7dd76
Deepak Dhakad <deepakdkd1188@gmail.com>
2024-02-15 15:36:47 +0530

    updated footer!

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
     <footer>
-
-    {/* <h1>Footer</h1> */}
+      <div className="contact">
+        <div className="icon">
+          <a href="https://github.com/DeepakDkd">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+              alt=""
+            />
+          </a>
+          <a href="https://instagram.com/captain_deepu_?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D">
+            <img
+              src="https://www.iconpacks.net/icons/4/free-icon-instagram-reels-13412.png"
+              alt=""
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/deepak-dhakad1?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
+            <img
+              src="https://www.iconpacks.net/icons/2/free-icon-linkedin-logo-2430.png"
+              alt=""
+            />
+          </a>
+          <a href="">
+            <img
+              src="https://www.iconpacks.net/icons/2/free-icon-youtube-logo-2431.png"
+              alt=""
+            />
+          </a>
+        </div>
+      </div>
+      <div className="copyright">
+        Copyright ©2024 All rights reserved |{" "}
+        <a href="mailto:deepakdkd1188@gmail.com">Developer 🧑‍💻</a>
+      </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
